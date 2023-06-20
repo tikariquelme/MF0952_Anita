cec1c9fc0add6cbe37134c99bca32f27ea54ee46
Anita <email@email.com>
2023-06-20 12:17:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id("username"),
+  email = id("email"),
+  password = id("password"),
+  form = id("form"),
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  engine(username, 0, "Nombre de usuario no puede ir vacio");
+  engine(email, 1, "Email no puede ir vacio");
+  engine(password, 2, "Password no puede ir vacio");
+});
+
+let engine = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
+
+    // icons
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+
+    // icons
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
